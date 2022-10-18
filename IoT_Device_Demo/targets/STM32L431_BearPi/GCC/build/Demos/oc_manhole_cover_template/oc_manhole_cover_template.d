b604b12823c6b577d2c65671f3059f1d63babbb9
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 18:06:07 +0800

    Add files via upload

---
+++ IoT_Device_Demo/targets/STM32L431_BearPi/GCC/build/Demos/oc_manhole_cover_template/oc_manhole_cover_template.d
@@ -0,0 +1,219 @@
+build/Demos/oc_manhole_cover_template/oc_manhole_cover_template.d \
+ build/Demos/oc_manhole_cover_template/oc_manhole_cover_template.o: \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Demos/oc_manhole_cover_template/oc_manhole_cover_template.c \
+ g:/iot/IoT_Device_Demo/iot_link/os/osal/osal.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/link_log.h \
+ g:/iot/IoT_Device_Demo/iot_link/iot_link_config.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/iot_config.h \
+ g:/iot/IoT_Device_Demo/iot_link/link_misc/link_misc.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/osal/osal_types.h \
+ g:/iot/IoT_Device_Demo/iot_link/oc/oc_lwm2m/oc_lwm2m_al/oc_lwm2m_al.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/E53_SC2/E53_SC2.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx_hal_conf.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/main.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_base.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/los_builddef.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_compiler.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_typedef.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_config.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/target_config.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_typedef.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l431xx.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/core_cm4.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_compiler.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_gcc.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/system_stm32l4xx.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/los_printf.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_config.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_list.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_errno.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_err.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_base.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_compiler.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_sys.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/arm-m/include/los_hwi.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/base/include/los_event.ph \
+ g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_event.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_qspi.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rng.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/i2c.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/LCD/lcd.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/gpio.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx_it.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT_Conf.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT_Conf.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/Inc/inv_mpu.h
+
+g:/iot/IoT_Device_Demo/iot_link/os/osal/osal.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/link_log.h:
+
+g:/iot/IoT_Device_Demo/iot_link/iot_link_config.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/iot_config.h:
+
+g:/iot/IoT_Device_Demo/iot_link/link_misc/link_misc.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/osal/osal_types.h:
+
+g:/iot/IoT_Device_Demo/iot_link/oc/oc_lwm2m/oc_lwm2m_al/oc_lwm2m_al.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/E53_SC2/E53_SC2.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx_hal_conf.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/main.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_base.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/los_builddef.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_compiler.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_typedef.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_config.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/target_config.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_typedef.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l431xx.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/core_cm4.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_compiler.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/common/cmsis/cmsis_gcc.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/system_stm32l4xx.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/OS_CONFIG/los_printf.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_config.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_list.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_errno.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_err.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_base.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_compiler.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_sys.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/arch/arm/arm-m/include/los_hwi.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/base/include/los_event.ph:
+
+g:/iot/IoT_Device_Demo/iot_link/os/liteos/include/los_event.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_adc.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_adc_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_qspi.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rng.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_spi_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_tim_ex.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+g:/iot/IoT_Device_Demo/drivers/third_party/ST/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/i2c.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/LCD/lcd.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/gpio.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/stm32l4xx_it.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT_Conf.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Inc/SEGGER_RTT_Conf.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/Hardware/Inc/inv_mpu.h:
