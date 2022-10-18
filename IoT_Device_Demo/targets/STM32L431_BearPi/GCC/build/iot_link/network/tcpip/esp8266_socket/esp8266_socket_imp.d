e7fb448e5cce7b3d4f7b92eb377a1389ec0d362a
Chenliang Xing <57756451+galaxyview@users.noreply.github.com>
2022-10-18 18:33:29 +0800

    Add files via upload

---
+++ IoT_Device_Demo/targets/STM32L431_BearPi/GCC/build/iot_link/network/tcpip/esp8266_socket/esp8266_socket_imp.d
@@ -0,0 +1,39 @@
+build/iot_link/network/tcpip/esp8266_socket/esp8266_socket_imp.d \
+ build/iot_link/network/tcpip/esp8266_socket/esp8266_socket_imp.o: \
+ g:/iot/IoT_Device_Demo/iot_link/network/tcpip/esp8266_socket/esp8266_socket_imp.c \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal_define.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal_types.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal_imp.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/link_log.h \
+ g:/iot/IoT_Device_Demo/iot_link/iot_link_config.h \
+ g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/iot_config.h \
+ g:/iot/IoT_Device_Demo/iot_link/at/at.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/osal/osal.h \
+ g:/iot/IoT_Device_Demo/iot_link/link_misc/link_misc.h \
+ g:/iot/IoT_Device_Demo/iot_link/os/osal/osal_types.h
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal_define.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal_types.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal_imp.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/link_log.h:
+
+g:/iot/IoT_Device_Demo/iot_link/iot_link_config.h:
+
+g:/iot/IoT_Device_Demo/targets/STM32L431_BearPi/iot_config.h:
+
+g:/iot/IoT_Device_Demo/iot_link/at/at.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/osal/osal.h:
+
+g:/iot/IoT_Device_Demo/iot_link/link_misc/link_misc.h:
+
+g:/iot/IoT_Device_Demo/iot_link/os/osal/osal_types.h:
---
+++ IoT_Device_Demo/targets/STM32L431_BearPi/GCC/build/iot_link/network/tcpip/sal/sal_netdb.d
@@ -0,0 +1,15 @@
+build/iot_link/network/tcpip/sal/sal_netdb.d \
+ build/iot_link/network/tcpip/sal/sal_netdb.o: \
+ g:/iot/IoT_Device_Demo/iot_link/network/tcpip/sal/sal_netdb.c \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal_define.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/sal_types.h \
+ g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal_define.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/sal_types.h:
+
+g:/iot/IoT_Device_Demo/iot_link/inc/link_endian.h:
